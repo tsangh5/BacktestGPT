0fe80ca35e8900942a7b9f761f6e11ffd295fd09
tsangbe3 <benjamin.tsang@mail.utoronto.ca>
2025-09-23 16:34:09 -0400

    change esLint config

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // Allow using 'any' type
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
